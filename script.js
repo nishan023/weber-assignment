f7753f8acfc09c10b69af53b9384bb93f26dbb4a
Nissan Dhakal <nissandhakal11@gmail.com>
2024-04-15 22:53:05 +0545

    images left

---
+++ script.js
@@ -0,0 +1,19 @@
+function qn_no_1() {
+    document.getElementById("Laptop").innerText = "My favorite laptop is MacBook Pro.";
+}
+
+function qn_no_2() {
+    document.getElementById("phone").innerText = "I prefer using iPhone as my smartphone.";
+}
+
+function qn_no_3() {
+    document.getElementById("Game").innerText = "My favorite game is Football.";
+}
+
+function qn_no_4() {
+    document.getElementById("Drink").innerText = "I love drinking coffee.";
+}
+
+function qn_no_5() {
+    document.getElementById("actress").innerText = "My favorite actress is Emma Watson.";
+}
